ac9995e052ee0ce4869632b774845d868e818085
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-03-17 12:40:48 +0530

    feat(slidingWindow): updated code

--- LeetCode/queue/slidingWindowMaximum.js
+++ LeetCode/queue/slidingWindowMaximum.js
@@ -13,30 +13,71 @@
 //       1  3  -1  -3 [5  3  6] 7       6
 //       1  3  -1  -3  5 [3  6  7]      7
 
-// pseudocode:        Brute force .
-// make a loop
-// use sliding window method
-// use one inner loop and check-
-// left will be 0 and window length is k
-// take the maxValue and push it to an array and move it
-// move the left pointer.
-// when the left pointer reaches array.length - 3, break loop
-// return the array
+// pseudocode:
+// take vars result and deque with empty arrays
+// make a loop with input array
+// inside loop, first check - the window is outside or not-
+// if(deque.length > 0 && deque[0] <= i-k) 
+// if it is, then shift(remove from start of deque).
+// second check (while loop)- if the nums[last index of deque] is greater or not with current nums[i]-
+// if(deque.length > 0 && nums[deque[deque.length-1]] <= nums[i])
+// if it is, then pop deque. till the end of the smaller values, continue popping
+// (removing the smaller values from the current window)
+// take the new greater element of the window- deque.push(i) 
+// atlast, check the window is started, - with the check if(i>= k-1), 
+// if it is, start to push the nums[deque[0]] to the result array
 
-var maxSlidingWindow = function (array, k) {
-    if (array.length === 0) return [];
-    if (array.length === 1) return [array[0]];
-
+let maxSlidingWindow = (nums, k) => {
     let result = [];
-    for (let i = 0; i <= array.length - k; i++) {
-        let max = -Infinity; // Reset max for each window
-        for (let j = i; j < i + k; j++) {
-            max = Math.max(array[j], max);
+    let deque = []; // This will store indices of potential max elements in the window
+
+    for (let i = 0; i < nums.length; i++) {
+        // Step 1: Remove indices from the deque if they are out of the current window
+        if (deque.length > 0 && deque[0] <= i - k) {
+            deque.shift(); // Remove from front if it's out of the window
         }
-        result.push(max);
+
+        // Step 2: Remove elements from the back of deque that are smaller than the current element
+        while (deque.length > 0 && nums[deque[deque.length - 1]] <= nums[i]) {
+            deque.pop(); // removing all smaller values from the end. with this,we're making the starting position index as big value
+        }
+
+        deque.push(i)  // Step 3: Add the current index to the deque
+
+        // Step 4: Add the maximum value of the window (front of the deque) to the result
+        if (i >= k - 1) {
+            result.push(nums[deque[0]])  // The largest element in the window is at the front of the deque
+        }
     }
     return result;
-};
+}
 
-console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
-console.log(maxSlidingWindow([1, -1], 1))
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
+
+
+
+// brute force. will not work in bigger arrays.
+// pseudocode:
+// make a loop of input array
+// take 2 vars left and max. left will be 0 initially
+// and move the left as per the input window in every iteration
+// and add the max value to the result array
+// return result
+
+// let maxSlidingWindow = (nums, k) => {
+//     let max = -Infinity;
+//     let result = [];
+//     for (let i = 0; i <= nums.length - k; i++) {
+//         for (let j = i; j < k + i; j++) {
+//             console.log('nums', nums[j])
+//             console.log('max', max)
+//             max = Math.max(max, nums[j]);
+//         }
+//         result.push(max);
+//         max = -Infinity;
+//     }
+//     return result;
+// }
+
+// console.log(maxSlidingWindow([1, -1], 1))
+// console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
