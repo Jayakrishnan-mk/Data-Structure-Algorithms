0eacd29cee4840d4e75cf8df93d00529262954c1
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-03-26 20:58:42 +0530

    feat(graph): new dsa added

--- LeetCode/tree/KthSmallestElement.js
+++ LeetCode/tree/KthSmallestElement.js
@@ -4,21 +4,27 @@
 // pseudocode:
 // check first edge case is root is null, then return 0
 // then use the recursion, to check the left side.
-// after that add it to the result array
+// after that add it to the result 
 // then right side
-// after the loop, return the result array's k'th value
+// after the loop, return the result
 
-var kthSmallest = function (root, k) {
-    if (!root) return 0;
-    let result = [];
-
+let kthSmallest = (root, k) => {
+    let count = 0;
+    let result;
     function traverse(node) {
+        if (!node) return null;
+
         if (node.left) traverse(node.left);
-        result.push(node.val);
+        count++;
+        if (count === k) {
+            result = node.val;
+            return;
+        }
         if (node.right) traverse(node.right);
     }
     traverse(root);
-    return result[k - 1];
-};
+    return result;
+}
 
-console.log(kthSmallest([5, 3, 6, 2, 4, null, null, 1], 3))
+console.log(kthSmallest([3, 1, 4, null, 2], 1))
+// console.log(kthSmallest([5, 3, 6, 2, 4, null, null, 1], 3))
