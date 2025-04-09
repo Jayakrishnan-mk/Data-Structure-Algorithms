bf520463508683f80c3f9d2916db9f175e3870f9
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-04-09 10:22:51 +0530

    feat(hm): hashmap problem added

--- Blind-75-List/containsDuplicate.js
+++ Blind-75-List/containsDuplicate.js
@@ -1,3 +1,20 @@
 
 // containsDuplicate - leetcode -easy
 
+// first make a map
+// loop the nums array and add the num to map
+// in an another loop, check the i is inside map with has property
+// if its not, after end of the loop, return false
+// if it is, return true
+
+let containsDuplicate = (nums) => {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) map.set(nums[i], i);
+        if (map.has(nums[i]) && map.get(nums[i]) !== i) return true;
+    }
+    return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]))
