5bcee7160c0ea650aef563e1640eb8075772e91c
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-05-24 10:47:43 +0530

    refactor(duplicate): codes removed

--- z.js
+++ z.js
@@ -1,20 +1,20 @@
 
 
 
-let moveZeroes = (nums) => {
-    if (nums.length === 1) return nums;
-    let pointer = 0;
+// let moveZeroes = (nums) => {
+//     if (nums.length === 1) return nums;
+//     let pointer = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            let temp = nums[i];
-            nums[i] = nums[pointer];
-            nums[pointer] = temp;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== 0) {
+//             let temp = nums[i];
+//             nums[i] = nums[pointer];
+//             nums[pointer] = temp;
 
-            pointer++;
-        }
-    }
-    return nums
-};
+//             pointer++;
+//         }
+//     }
+//     return nums
+// };
 
-console.log(moveZeroes(nums = [1, 2, 0, 3, 0, 4]))
+// console.log(moveZeroes(nums = [1, 2, 0, 3, 0, 4]))
