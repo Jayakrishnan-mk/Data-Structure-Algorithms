808871c950253649a2f5105632daa5de70cd57d8
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-07-03 23:17:00 +0530

    Adds function to remove specified elements from array
    Introduces a function that removes all occurrences of a given value from an array while preserving order. Adjusts array length to reflect changes and ensures efficient in-place element manipulation.
    
    Includes an example invocation and output demonstration.

--- z.js
+++ z.js
@@ -1,10 +1,37 @@
 
 
-(function () {
-    var a;          // declaration hoisted
-    console.log(a); // a is undefined at this point
-    a = 10;         // value assigned later
-})();
+// Your input
+// [3,2,2,3], 3
+
+// Expected answer
+// [2,2]
 
 
+var removeElement = function (nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums[i] = '_';
+        } else {
+            k++;
+        }
+    }
+    let pointer = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== '_') {
+            [nums[pointer], nums[i]] = [nums[i], nums[pointer]];
+            pointer++;
+        }
+    }
+    let indx = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === '_') {
+            indx = i;
+        }
+    }
+    nums.length = k;
+    return nums;
+};
 
+console.log(removeElement([3, 2, 2, 3], 3));
+// o/p - [2,2]
