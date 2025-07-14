51717f63b38261749df5c76946a6bb310734fc37
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-07-14 23:40:48 +0530

    Removes duplicates from a sorted array
    Introduces a function to efficiently remove duplicates from a sorted array by shifting unique values to the front and resizing the array to exclude duplicates. Improves array handling and reduces redundancy.

--- z.js
+++ z.js
@@ -1,15 +1,25 @@
 
 
+// remove duplicates from sorted array
 
 
-let a = 1;
 
-function foo() {
-    a = 10;
-    return a;
-    function a() { }
-}
 
-console.log(foo());
-console.log(a);
+// make a pointer in starting
+// make the unique values to first
+// then pointer++
+// atlast, return the array
+
+var removeDuplicates = function(nums) {  
+   let pointer = 0;
+    
+    for(let i = 0; i<nums.length ; i++) {
+        if(i === 0 || nums[i] !== nums[i-1]) {
+            nums[pointer] = nums[i] ;
+            pointer++;
+        }
+    }
+    nums.length = pointer;
+};
+
 
