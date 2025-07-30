4950704b8a25a3d1409873a931fba20d2b50426f
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-07-30 22:46:00 +0530

    Adds function to check if array contains double values
    Introduces a function that iterates over an array to determine
    if any element is twice the value of another. Improves utility
    for numerical array validation by returning a boolean result.

--- z.js
+++ z.js
@@ -1,2 +1,13 @@
 
 
+var checkIfExist = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j && arr[i] === 2 * arr[j]) {
+                // console.log(i, j);
+                return true;
+            }
+        }
+    }
+    return false;
+};
