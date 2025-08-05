de77ee2fd7e043aa29c692ad11baf3e168b113f3
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-08-05 23:45:24 +0530

    Comments out sortedSquares function and usage
    Temporarily disables the sortedSquares function and its usage by converting the code to comments.
    
    This change may indicate ongoing debugging, refactoring, or deprecation efforts while preserving the original implementation for reference.

--- z.js
+++ z.js
@@ -1,25 +1,25 @@
 
 
-var sortedSquares = function (nums) {
-    let first = 0, last = nums.length - 1;
-    let result = Array(nums.length);
-    let index = nums.length - 1;
+// var sortedSquares = function (nums) {
+//     let first = 0, last = nums.length - 1;
+//     let result = Array(nums.length);
+//     let index = nums.length - 1;
 
-    while (first <= last) {
-        let firstVal = nums[first] * nums[first];
-        let lastVal = nums[last] * nums[last];
+//     while (first <= last) {
+//         let firstVal = nums[first] * nums[first];
+//         let lastVal = nums[last] * nums[last];
 
-        if (firstVal >= lastVal) {
-            result[index] = firstVal;
-            first++;
-        } else {
-            result[index] = lastVal;
-            last--;
-        }
-        index--;
-    }
-    return result;
-}
+//         if (firstVal >= lastVal) {
+//             result[index] = firstVal;
+//             first++;
+//         } else {
+//             result[index] = lastVal;
+//             last--;
+//         }
+//         index--;
+//     }
+//     return result;
+// }
 
-console.log(sortedSquares([-7, -3, 2, 3, 11]))
-// [ 4, 9, 9, 49, 121 ]
+// console.log(sortedSquares([-7, -3, 2, 3, 11]))
+// // [ 4, 9, 9, 49, 121 ]
