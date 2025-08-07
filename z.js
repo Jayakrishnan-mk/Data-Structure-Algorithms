02fce629fd9864b146b947a5c70697e13d751295
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-08-07 23:57:06 +0530

    Replaces squared sorting logic with max replacement logic
    Replaces the `sortedSquares` function with a new `replaceElements` function. The new logic calculates the maximum value to the right of each element in the array and updates the array in place. Removes the unused squared sorting functionality.

--- z.js
+++ z.js
@@ -1,25 +1,18 @@
 
 
-var sortedSquares = function (nums) {
-    let first = 0, last = nums.length - 1;
-    let result = Array(nums.length);
-    let index = nums.length - 1;
 
-    while (first <= last) {
-        let firstVal = nums[first] * nums[first];
-        let lastVal = nums[last] * nums[last];
 
-        if (firstVal >= lastVal) {
-            result[index] = firstVal;
-            first++;
-        } else {
-            result[index] = lastVal;
-            last--;
-        }
-        index--;
+
+var replaceElements = function (arr) {
+    let max = -1;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let current = arr[i];
+        arr[i] = max;
+        max = Math.max(max, current);
     }
-    return result;
-}
+    return arr;
+};
 
-console.log(sortedSquares([-7, -3, 2, 3, 11]))
-// [ 4, 9, 9, 49, 121 ]
+console.log(replaceElements([17, 18, 5, 4, 6, 1]))
+
