6361855868701df4cf7e645e549a0c93c4f53f6a
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-09-05 21:19:25 +0530

    Adds a function to extract maximum values from nested arrays
    Introduces a function that iterates through nested arrays to find and collect the maximum values from each sub-array. Includes a sample call to demonstrate functionality.

--- z.js
+++ z.js
@@ -1,2 +1,23 @@
 
 
+
+
+
+let presentProblem = (array) => {
+    let max = 0;
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let innerArray = array[i];
+        for (let j = 0; j < innerArray.length; j++) {
+            if (innerArray[j] < innerArray[j + 1]) {
+                max = innerArray[j + 1];
+            }
+        }
+        result.push(max);
+        max = 0;
+    }
+    return result;
+}
+console.log(presentProblem([[3, 5, 6], [3, 9, 1], [2, 0, 1, 2]]))
+
+
